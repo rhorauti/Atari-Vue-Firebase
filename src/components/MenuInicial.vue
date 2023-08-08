718736c4f218caf7caf8e706ab796b927a0c15ad
rafael-horauti <rafael.horauti@uiclap.com>
2023-08-07 22:12:49 -0300

    Alteração layout geral

---
+++ src/components/MenuInicial.vue
@@ -0,0 +1,30 @@
+<script>
+
+
+</script>
+
+<style>
+
+
+</style>
+
+<template>
+
+    <section>
+      <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <router-link to="/clientes" class="navbar-item">
+            <img src="@/img/atari.png" width="60">
+          </router-link>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-start">
+              <router-link to="/clientes" class="navbar-item">Clientes</router-link>
+              <router-link to="/fornecedores" class="navbar-item">Fornecedores</router-link>
+              <router-link to="" class="navbar-item">Empresa</router-link>
+          </div>
+        </div>
+      </nav>
+    </section>
+    
+    </template>
